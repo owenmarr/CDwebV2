358a1939698187521705a829c9f36eb9599fcc14
owenmarr <56004548+owenmarr@users.noreply.github.com>
2020-04-18 21:57:37 -0400

    Add files via upload

---
+++ JS/display.js
@@ -0,0 +1,6 @@
+window.onload = go;
+function go(){
+  var make = document.querySelector("#make").innerHTML
+  var make = document.querySelector("#model").innerHTML
+  document.querySelector("#disp-result-text").innerHTML = ' * ';
+}
